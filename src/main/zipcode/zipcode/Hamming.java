dfb4ced53b5678d110fdd6f3e13e18ae12e2012c
val <valfragier@gmail.com>
2019-11-14 13:21:53 -0500

    Complete

---
+++ src/main/zipcode/zipcode/Hamming.java
@@ -0,0 +1,26 @@
+package zipcode;
+
+public class Hamming {
+    String s;
+    String s1;
+
+    //Constructor
+    public Hamming(String s, String s1) {
+        if (s.length() != s1.length() ){
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+
+        this.s = s;
+        this.s1 = s1;
+    }
+
+    public int getHammingDistance() {
+        int counter = 0;
+        for (int i = 0; i < s.length() ; i++) {
+            if (s.charAt(i) != s1.charAt(i)){
+                counter ++;
+            }
+        }
+        return counter;
+    }
+}
